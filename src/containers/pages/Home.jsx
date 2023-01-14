d94fe480309876ed9374f22daf6635b52d88a7d0
ShoQui <sho-qui@hotmail.com>
2023-01-13 22:27:21 -0500

    Home_Edit

--- src/containers/pages/Home.jsx
+++ src/containers/pages/Home.jsx
@@ -1,5 +1,9 @@
+import BlogList from "components/home/BlogList"
+import CTA from "components/home/CTA"
+import Features from "components/home/Features"
 import Header from "components/home/Header"
 import Incentives from "components/home/Incentives"
+import LogoCloud from "components/home/LogoCloud"
 import Portafolio from "components/home/Portafolio"
 import Footer from "components/navigation/Footer"
 import Navbar from "components/navigation/Navbar"
@@ -12,9 +16,15 @@ function Home(){
         <Layout>                        
                 <Header/>  
                 <Navbar/>
-                <Incentives/>
-                <Portafolio/>           
-                <Footer/>
+                    <div>
+                        <Incentives/>
+                        <Portafolio/>
+                        <Features/>
+                        <CTA/>
+                        <LogoCloud/>
+                        <BlogList/>       
+                    </div>  
+                <Footer/>                
             
         </Layout>
     )
